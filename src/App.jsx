e466a7f2a6ce1cbb0e41c1e9ce04ca53c7b8e72d
phulrepalak <132000502+phulrepalak@users.noreply.github.com>
2026-04-01 14:08:45 +0530

    refactor routing structure in App component for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -12,33 +12,41 @@ import SearchResults from "./pages/SearchResults";
 import AccountCreated from "./pages/AccountCreated";
 import CompleteProfile from "./pages/CompleteProfile";
 
+// Admin Pages
+import AdminDashboard from "./pages/AdminDashboard"; // Isme Add Bus ka form hai
+// import ManageBus from "./pages/ManageBus";           // Future page for edit/delete
+// import ViewBookings from "./pages/ViewBookings";     // Future page to see all tickets
+
 function App() {
   return (
     <Router>
       <Navbar />
 
       <Routes>
-        {/* 1. Default Page ab Home khulega */}
+        {/* --- COMMON ROUTES --- */}
         <Route path="/" element={<Home />} />
-        
-        {/* 2. Login Page ab /login path par chalagaya hai */}
-        <Route path="/login" element={<LoginPage />} />
-        
-        <Route path="/otp" element={<OTPPage />} />
-        
-        {/* /home ko bhi rehne dete hain redirection ke liye */}
         <Route path="/home" element={<Home />} />
-        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/otp" element={<OTPPage />} />
         <Route path="/search" element={<SearchResults />} />
-
         <Route path="/account-created" element={<AccountCreated />} />
         <Route path="/profile" element={<CompleteProfile />} />
 
-        {/* 3. Galat URL par seedha Home par bhej dega */}
+        {/* --- ADMIN ONLY ROUTES --- */}
+        {/* Jab Navbar mein 'Add Bus' click hoga toh ye khulega */}
+        <Route path="/admin" element={<AdminDashboard />} /> 
+        
+        {/* Manage Bus page ka path */}
+        {/* <Route path="/manage-bus" element={<ManageBus />} /> */}
+        
+        {/* Saari bookings dekhne ka path */}
+        {/* <Route path="/view-bookings" element={<ViewBookings />} /> */}
+
+        {/* --- REDIRECTS --- */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App;   
